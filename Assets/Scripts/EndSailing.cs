bbca99e80a79e3d9d0168d05e79ee57715058cab
Magnus Bøndergaard Poulsen <202008368@post.au.dk>
2024-11-27 15:52:21 +0100

    added end screen

--- Assets/Scripts/EndSailing.cs
+++ Assets/Scripts/EndSailing.cs
@@ -4,16 +4,16 @@ using UnityEngine.SceneManagement;
 public class EndSailing : MonoBehaviour
 {
     // Define the Z position threshold
-    public float targetZPosition = 42f;
+    public float targetXPosition = 42f;
 
     // Update is called once per frame
     void Update()
     {
         // Check if the object's Z position has crossed the target Z position
-        if (transform.position.z >= targetZPosition)
+        if (transform.position.x >= targetXPosition)
         {
             // Trigger the scene change
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 }
